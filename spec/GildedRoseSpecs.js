5210e90be68467b1bbfc0509f42d68ca090d8e02
Guille <guille@ggalmazor.com>
2012-07-04 22:31:32 +0200

    Undo changes from aos2012's solution

--- spec/GildedRoseSpecs.js
+++ spec/GildedRoseSpecs.js
@@ -1,43 +1,5 @@
 describe("GildedRose shop manager", function () {
-  describe("Situación incial (tests de cobertura para refactor)", function () {
-
-    it("actualiza la calidad y el sellIn de los productos adecuadamente", function () {
-      var items = [];
-      items.push(new Item("+5 Dexterity Vest", 10, 20));
-      items.push(new Item("Aged Brie", 2, 0));
-      items.push(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-      items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-      items.push(new Item("Conjured Mana Cake", 3, 6));
-      items = GildedRose.updateQuality(items);
-      [
-        {q:19, s:9},
-        {q:1, s:1},
-        {q:80, s:0},
-        {q:21, s:14},
-        {q:4, s:2}
-      ].forEach(function (testCase, idx) {
-            expect(items[idx].quality).toBe(testCase.q);
-            expect(items[idx].sellIn).toBe(testCase.s);
-          });
-    });
-    it("actualiza la calidad y el sellIn de los productos adecuadamente cuando hemos superado el sellIn", function () {
-      var items = [];
-      items.push(new Item("+5 Dexterity Vest", 0, 20));
-      items.push(new Item("Aged Brie", 0, 0));
-      items.push(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-      items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20));
-      items.push(new Item("Conjured Mana Cake", 0, 6));
-      items = GildedRose.updateQuality(items);
-      [
-        {q:18, s:-1},
-        {q:2, s:-1},
-        {q:80, s:0},
-        {q:0, s:-1},
-        {q:2, s:-1}
-      ].forEach(function (testCase, idx) {
-            expect(items[idx].quality).toBe(testCase.q);
-            expect(items[idx].sellIn).toBe(testCase.s);
-          });
-    });
+  it("does some stuff", function() {
+     expect(1).toBe(1);
   });
 });
--- src/Item.js
+++ src/Item.js
@@ -1,30 +1,5 @@
 var Item = function (name, sellIn, quality) {
-  var maxQuality = 50;
-  var minQuality = 0;
+  this.name = name;
   this.sellIn = sellIn;
   this.quality = quality;
-
-  this.decreaseQuality = function () {
-    this.quality -= 1;
-  };
-
-  this.increaseQuality = function () {
-    this.quality += 1;
-  };
-
-  this.is = function (otherName) {
-    return otherName == name;
-  };
-
-  this.resetQuality = function () {
-    this.quality = 0;
-  };
-
-  this.hasReachedMaxQuality = function () {
-    return this.quality >= maxQuality;
-  };
-
-  this.hasReachedMinQuality = function () {
-    return this.quality === minQuality;
-  };
 };

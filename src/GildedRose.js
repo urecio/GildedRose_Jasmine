f0dbb04ac2b7f2d0a8225b2de5f49b6409dceb1b
Guille <guille@ggalmazor.com>
2012-07-05 17:54:00 +0200

    All magic numbers and strings are gone

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -11,52 +11,62 @@ var GildedRose = function () {
 };
 GildedRose.updateQuality = function (items) {
   for (var i = 0; i < items.length; i++) {
-    if ("Aged Brie" != items[i].name && "Backstage passes to a TAFKAL80ETC concert" != items[i].name) {
-      if (items[i].quality > 0) {
-        if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-          items[i].quality = items[i].quality - 1
+    var item = items[i];
+    var cheese = "Aged Brie";
+    var concertPass = "Backstage passes to a TAFKAL80ETC concert";
+    var legendary = "Sulfuras, Hand of Ragnaros";
+    var conjured = "Conjured Mana Cake";
+    var minQuality = 0;
+    var maxQuality = 50;
+    var minSellIn = 0;
+    var firstTierSellIn = 11;
+    var secondTierSellIn = 6;
+    if (cheese != item.name && concertPass != item.name) {
+      if (item.quality > minQuality) {
+        if (legendary != item.name) {
+          item.quality = item.quality - 1
         }
-        if ("Conjured Mana Cake" == items[i].name) {
-          items[i].quality = items[i].quality - 1
+        if (conjured == item.name) {
+          item.quality = item.quality - 1
         }
       }
     } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if ("Backstage passes to a TAFKAL80ETC concert" == items[i].name) {
-          if (items[i].sellIn < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
+      if (item.quality < maxQuality) {
+        item.quality = item.quality + 1;
+        if (concertPass == item.name) {
+          if (item.sellIn < firstTierSellIn) {
+            if (item.quality < maxQuality) {
+              item.quality = item.quality + 1
             }
           }
-          if (items[i].sellIn < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
+          if (item.sellIn < secondTierSellIn) {
+            if (item.quality < maxQuality) {
+              item.quality = item.quality + 1
             }
           }
         }
       }
     }
-    if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-      items[i].sellIn = items[i].sellIn - 1;
+    if (legendary != item.name) {
+      item.sellIn = item.sellIn - 1;
     }
-    if (items[i].sellIn < 0) {
-      if ("Aged Brie" != items[i].name) {
-        if ("Backstage passes to a TAFKAL80ETC concert" != items[i].name) {
-          if (items[i].quality > 0) {
-            if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-              items[i].quality = items[i].quality - 1
+    if (item.sellIn < minSellIn) {
+      if (cheese != item.name) {
+        if (concertPass != item.name) {
+          if (item.quality > minQuality) {
+            if (legendary != item.name) {
+              item.quality = item.quality - 1
             }
-            if ("Conjured Mana Cake" == items[i].name) {
-              items[i].quality = items[i].quality - 1
+            if (conjured == item.name) {
+              item.quality = item.quality - 1
             }
           }
         } else {
-          items[i].quality = items[i].quality - items[i].quality
+          item.quality = item.quality - item.quality
         }
       } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
+        if (item.quality < maxQuality) {
+          item.quality = item.quality + 1
         }
       }
     }

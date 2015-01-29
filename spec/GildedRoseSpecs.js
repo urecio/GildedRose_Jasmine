81f28bb805ad259d53ffc7798a04b52401c1a512
Unai Recio <unai.recio@agcpartners.co.uk>
2015-01-29 18:06:42 +0100

    test(example): first example

--- spec/GildedRoseSpecs.js
+++ spec/GildedRoseSpecs.js
@@ -1,5 +1,8 @@
 describe("GildedRose shop manager", function () {
-  it("does some stuff", function() {
-     expect(1).toBe(1);
+  it("should decrement quality and sellin by one when updating quality", function() {
+     var pepsi = Item('pepsi', 50 ,50);
+     GildedRose.updateQuality(pepsi);
+     expect(pepsi.sellIn).toBe(49);
+     expect(pepsi.quality).toBe(49);
   });
 });

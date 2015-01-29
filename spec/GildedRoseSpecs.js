c0c12cdca23a2cb2db7ad2ff5fcf099121658d58
Unai Recio <unai.recio@agcpartners.co.uk>
2015-01-29 19:14:55 +0100

    chore(initial karma set): - Adds also the first test

--- spec/GildedRoseSpecs.js
+++ spec/GildedRoseSpecs.js
@@ -1,8 +1,8 @@
 describe("GildedRose shop manager", function () {
   it("should decrement quality and sellin by one when updating quality", function() {
-     var pepsi = Item('pepsi', 50 ,50);
-     GildedRose.updateQuality(pepsi);
-     expect(pepsi.sellIn).toBe(49);
-     expect(pepsi.quality).toBe(49);
+     var items = [new Item('pepsi', 50 ,50)];
+     items = GildedRose.updateQuality(items);
+     expect(items[0].sellIn).toBe(49);
+     expect(items[0].quality).toBe(49);
   });
 });
